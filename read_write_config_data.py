08cad58b0114903b8c75cb10a60efe810c8cfbe1
Bernardo Lanza <87471233+bernardolanza93@users.noreply.github.com>
2022-05-25 18:25:45 +0200

    aggiunta file per aggiungere nuovi esercizi
    cambiare desktop scrivania e chiamare la funzione

---
+++ read_write_config_data.py
@@ -0,0 +1,70 @@
+# Import pandas
+import pandas as pd
+import EVA
+import os
+import configparser
+
+
+
+
+def check_new_exercise_in_excel_file():
+    # Load the xlsx file
+    ini_ex_file='exercise_info.ini'
+
+    desktop_name = 'Scrivania'
+    #current_wd= os.getcwd()
+    desktop = os.path.normpath(os.path.expanduser("~/" + desktop_name))
+    excel_data = pd.read_excel(desktop + '/esercizi.xlsx')
+    config = configparser.ConfigParser()
+    config.read(ini_ex_file)
+    sections = config.sections()
+    print(sections)
+    # Print the content
+    IDS = (excel_data["id"]).tolist()
+    IDS =  [x for x in IDS if pd.notnull(x)]
+    exercise = (excel_data["exercise"]).tolist()
+    target = (excel_data["target"]).tolist()
+    target =  [x for x in target if pd.notnull(x)]
+
+    #remove nan object
+    exercise =  [x for x in exercise if pd.notnull(x)]
+
+    missing_ex = list(set(exercise).difference(sections))
+
+    for i in range(len(missing_ex)):
+
+        config.add_section(missing_ex[i])
+
+    #write ini
+    newini = open(ini_ex_file, 'w')
+    config.write(newini)
+
+    #take all new and set
+
+    sections = config.sections()
+    print(sections)
+
+    while len(IDS) < len(exercise):
+        IDS.append("")
+
+    while len(target) < len(exercise):
+        target.append("")
+
+    for k in range(len(exercise)):
+        #print(target[k])
+        config.set(exercise[k], 'target_bar', str(target[k]))
+        config.set(exercise[k], 'id', str(int(IDS[k])))
+
+    newini = open(ini_ex_file, 'w')
+    config.write(newini)
+
+    return missing_ex
+
+
+
+
+
+
+print("new exercise:",check_new_exercise_in_excel_file())
+
+
