ff92ef20d3f2d7cd31ed9cf6b8725b0f9d1c25cf
Bernardo Lanza <87471233+bernardolanza93@users.noreply.github.com>
2022-05-26 14:47:24 +0200

    robustness of empty lines

--- read_write_config_data.py
+++ read_write_config_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import EVA
 import os
 import configparser
+import numpy as np
 
 
 
@@ -15,25 +16,38 @@ def check_new_exercise_in_excel_file():
     #current_wd= os.getcwd()
     desktop = os.path.normpath(os.path.expanduser("~/" + desktop_name))
     excel_data = pd.read_excel(desktop + '/esercizi.xlsx')
+
+    #sostituisco i nan con spazi vuoti
+    excel_data= excel_data.replace(np.nan, '', regex=True)
+    print(excel_data)
     config = configparser.ConfigParser()
     config.read(ini_ex_file)
     sections = config.sections()
-    print(sections)
     # Print the content e togli tutti i nan
     IDS = (excel_data["id"]).tolist()
-    IDS =  [x for x in IDS if pd.notnull(x)]
     exercise = (excel_data["exercise"]).tolist()
+
     target = (excel_data["target"]).tolist()
-    target =  [x for x in target if pd.notnull(x)]
 
     #remove nan object
-    exercise =  [x for x in exercise if pd.notnull(x)]
-#cerca eseercizi mancanti
+    #cerca eseercizi mancanti
     missing_ex = list(set(exercise).difference(sections))
+
+
 
     for i in range(len(missing_ex)):
 
-        config.add_section(missing_ex[i])
+
+        if missing_ex[i] != '':
+            config.add_section(missing_ex[i])
+        else:
+
+            print("empty exercise in missing, skip")
+
+    missing_ex= [x for x in missing_ex if x]
+    print("missing",missing_ex)
+
+
 
     #write ini
     newini = open(ini_ex_file, 'w')
@@ -42,7 +56,7 @@ def check_new_exercise_in_excel_file():
     #take all new and set
 
     sections = config.sections()
-    print(sections)
+    #print(sections)
 #se dovessero esserci dei parametri non assegnati , assegno spazio vuoto (poi il parser mette default
     while len(IDS) < len(exercise):
         IDS.append("")
@@ -50,19 +64,35 @@ def check_new_exercise_in_excel_file():
     while len(target) < len(exercise):
         target.append("")
 #inserisce parametri di sezione guardando dall excel
-    for k in range(len(exercise)):
-        #print(target[k])
-        config.set(exercise[k], 'target_bar', str(target[k]))
-        config.set(exercise[k], 'id', str(int(IDS[k])))
+    nex = len(exercise)
+    print(exercise)
+    for k in range(nex):
+
+
+
+
+        if exercise[k] != '':
+            print(exercise[k])
+            if target[k] == '':
+                config.set(exercise[k], 'target_bar', '')
+                print("empty target")
+            else:
+                config.set(exercise[k], 'target_bar', str(target[k]))
+
+            if IDS[k] == '':
+                config.set(exercise[k], 'id', '')
+                print("empty id")
+            else:
+                config.set(exercise[k], 'id', str(int(IDS[k])))
+        else:
+            print("remove incomplete line // no exercise:",exercise[k],target[k],IDS[k])
+
+
 
     newini = open(ini_ex_file, 'w')
     config.write(newini)
 
     return missing_ex
-
-
-
-
 
 
 print("new exercise:",check_new_exercise_in_excel_file())
